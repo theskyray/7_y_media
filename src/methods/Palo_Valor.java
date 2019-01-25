6c94d7cdb4760b73845f87cb158b339e9fd4cd70
al036066 <al036066@pc2208>
2019-01-25 20:02:33 +0100

    FORMATO DE ENUMERADOS

--- src/methods/Palo_Valor.java
+++ src/methods/Palo_Valor.java
@@ -1,6 +1,27 @@
 package methods;
 
 public enum Palo_Valor {
-	public enum Palo {OROS, COPAS, ESPADAS, BASTOS}
-	public enum Valor {As, Dos, Tres, Cuatro, Cinco, Seis, Siete, Sota, Caballo, Rey}
+	public enum Palo {
+		
+		OROS,
+		COPAS,
+		ESPADAS,
+		BASTOS
+		
+	}
+	
+	public enum Valor {
+	
+		As,
+		Dos,
+		Tres,
+		Cuatro,
+		Cinco,
+		Seis,
+		Siete,
+		Sota,
+		Caballo,
+		Rey
+		
+	}
 }
