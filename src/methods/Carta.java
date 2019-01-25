de977432ea9687e9905cc78c75f1a16064239786
adrian.santosgonzalez <adrian.santosgonzalez@pc2206>
2019-01-25 20:15:28 +0100

    carat metodo

--- src/methods/Carta.java
+++ src/methods/Carta.java
@@ -5,7 +5,7 @@ public class Carta {
 	private int Palo;
 	private int Valor;
 	
-	public Carta(int Palo, int Valor) {
+	public Carta(int Palo,  int Valor) {
 		this.Palo = Palo;
 		this.Valor = Valor;
 	}
