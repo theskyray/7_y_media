6c9b695664615645e2753c2cae344a2c6323b809
jose.martincalvo <jose.martincalvo@pc2207>
2019-01-25 20:40:47 +0100

    baraja 	Crear carta

--- src/methods/Baraja.java
+++ src/methods/Baraja.java
@@ -2,5 +2,14 @@ package methods;
 
 public class Baraja {
 
+	private Baraja baraja[] = new Baraja [40];
+	
+	public Baraja () {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 10; j++) {
+				Carta carta = new Carta (i, j);
+			}
+		}
+	}
 
 }
