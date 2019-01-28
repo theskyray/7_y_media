4f1398f105374aeae65ada6e2b3811960d622b77
jose.martincalvo <jose.martincalvo@pc2207>
2019-01-28 16:36:54 +0100

    sacarCarta

--- src/main/Main_act_7_1.java
+++ src/main/Main_act_7_1.java
@@ -3,8 +3,14 @@ package main;
 public class Main_act_7_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		for (int i = 0; i < 100; i++) {
+			int j = (int) (Math.random()*40);
+			System.out.println(j);
+		}
+
+		
+		
 	}
 
 }
--- src/methods/Baraja.java
+++ src/methods/Baraja.java
@@ -2,14 +2,28 @@ package methods;
 
 public class Baraja {
 
-	private Baraja baraja[] = new Baraja [40];
+	private Carta carta;
+	private Carta baraja[] = new Carta [40];
 	
 	public Baraja () {
+		int k = 0;
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 10; j++) {
-				Carta carta = new Carta (i, j); 
+				baraja[k] = new Carta (i, j);
+				k++;
 			}
 		}
 	}
+	
+	public void sacarCarta() {
+		int i = (int) (Math.random()*40);
+		while (baraja[i].isCartaEnBaraja() == false) {
+			i = (int) (Math.random()*40);
+		}
+		baraja[i].setCartaEnBaraja(false);
+	}
+	
+	
+	
 
 }
