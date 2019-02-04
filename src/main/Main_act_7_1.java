29c0f3be56c46a2d323c420ef45875fb9ad13b63
al036066 <al036066@pc2208>
2019-02-04 17:08:09 +0100

    main

--- src/main/Main_act_7_1.java
+++ src/main/Main_act_7_1.java
@@ -10,11 +10,6 @@ public class Main_act_7_1 {
 		Baraja baraja = new Baraja();
 		Jugador jugador = new Jugador();
 		
-		System.out.println(jugador);
-		
-		jugador.pedirCarta();
-		
-		System.out.println(jugador);
 		
 	}
 
